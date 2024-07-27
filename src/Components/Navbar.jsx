b47b396bceb8b6dff91ae47efd44e3d3a1b34d9b
AbdulAhadera <abdulahad.era@gmail.com>
2024-07-27 06:06:42 -0700

    second commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import '../Css/Navbar.css';
 import logo from '../Assets/Innova360.png';
 import { Link } from 'react-router-dom';
-import { RxCross2 } from "react-icons/rx";
-import { FaBars, FaBold } from "react-icons/fa6";
+import 'animate.css';
 
 
 function Navbar() {
 
   const [isCross, setIsCross] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
 
   const toggleCross = () => {
     setIsCross(!isCross);
+    setOpenDrawer(!openDrawer)
   };
 
   return (
@@ -41,17 +42,33 @@ function Navbar() {
         <div className='nav-elem'>
           <img src={logo} className='nav-img' />
         </div>
-
         <div className='nav-elem2'>
+          <div className='nav-btn-87'>
           <button className='button-87'>Book an Apointment</button>
-          <div style={{margin:"5px"}}>
+          </div>
+
+          <div style={{ margin: "5px" }}>
             <div className={`bar-container ${isCross ? 'cross' : ''}`} onClick={toggleCross}>
               <div className="bar bar1"></div>
               <div className="bar bar2"></div>
             </div>
           </div>
+
         </div>
       </div>
+
+      <div className={openDrawer ? 'drawer-box animate__animated animate__slideInUp' : 'drawer-box animate__animated animate__slideOutDown'}>
+        <div className='menu'>
+          <p className='drawer-text'>Menu</p>
+        </div>
+        <div className='drawer-link-div'><Link className='drawer-link'>Home</Link></div>
+        <div className='drawer-link-div'><Link className='drawer-link'>Services</Link></div>
+        <div className='drawer-link-div'><Link className='drawer-link'>About us</Link></div>
+        <div className='drawer-link-div'><Link className='drawer-link'>Contact us</Link></div>
+        <button className='button-87'>Book an Apointment</button>
+      </div>
+
+
 
 
     </div >
